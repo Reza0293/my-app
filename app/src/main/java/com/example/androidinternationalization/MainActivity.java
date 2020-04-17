c84e8bd90de0360b157399605989ebd5217a81e6
Reza0293 <piyrw0293@gmail.com>
2020-04-17 18:24:56 +0430

    Initial commit

---
+++ app/src/main/java/com/example/androidinternationalization/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.androidinternationalization;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.androidinternationalization.R;
+
+import org.jdom2.Document;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Document DOC = new Document();
+
+    }
+
+    }
+
